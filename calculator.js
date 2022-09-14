066904af53d9d12f1398f86e2bea5c8123feef6b
rosemutai <chepngetichrose2030@gmail.com>
2022-09-14 23:29:03 +0300

    link js file

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let currentNum = document.getElementById('display-area')
+let results = document.getElementById('results')
+
+let numSeven = document.getElementById('seven')
+
+let num
+
+function appendNumber (){
+    const value = Number(e.target.innerHTML)
+    currentNum.append(value)
+
+}
